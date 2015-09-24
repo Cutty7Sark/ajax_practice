f9ee7c0e408dcfd8245da4d5ac2a9a9cd79421a8
cutty7sark <cutty7sark@gmail.com>
2015-09-24 21:02:47 +0300

    add

---
+++ main.js
@@ -0,0 +1,31 @@
+var headUr1 = '/ajax';
+var htmlList = ['index.html', 'pageone.html', 'pagetwo.html'];
+
+$.ajax({
+	url: headUr1 = 'pageone.html',
+	success: function(data) {
+		$('body').append(data);
+		//document.body.appendChildren(data);
+	},
+	error: function(data) {
+	  console.error(data);
+	}
+});
+
+htmlList.forEach( loadHtmlFiles);
+
+
+// $.ajax({
+// 	dataType: 'json',
+// 	url: headUr1 + element,
+// 	success: function(data) {
+// 		var wrapDiv = $('<div>');
+// 		wrapDiv.html('<h2>' + data.title + '</h2><p>' + data.desc + </p>);
+// 		$('.wrapper').append(wrapDiv);
+// 		//document.body.appendChildren(data);
+// 	},
+// 	error: function(data) {
+// 	  console.error(data);
+// 	}
+// });
+// jsonList.forEach( loadJSONFiles);
