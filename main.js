79d5eeecedaeb242bf756c3f501fa3a596034752
Kate <cutty7sark@gmail.com>
2015-10-01 20:23:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var headUr1 = '/ajax_practice';
 var htmlList = ['index1.html', 'pageone.html', 'pagetwo.html'];
 
 $.ajax({
-	url: headUr1 = 'pageone.html',
+	url: headUrl + htmlList[1],
 	success: function(data) {
-		$('body').append(data);
+		$('#results_one').load('pageone.html');
 		//document.body.appendChildren(data);
 	},
 	error: function(data) {
@@ -12,20 +12,13 @@ $.ajax({
 	}
 });
 
-htmlList.forEach( loadHtmlFiles);
-
-
-// $.ajax({
-// 	dataType: 'json',
-// 	url: headUr1 + element,
-// 	success: function(data) {
-// 		var wrapDiv = $('<div>');
-// 		wrapDiv.html('<h2>' + data.title + '</h2><p>' + data.desc + </p>);
-// 		$('.wrapper').append(wrapDiv);
-// 		//document.body.appendChildren(data);
-// 	},
-// 	error: function(data) {
-// 	  console.error(data);
-// 	}
-// });
-// jsonList.forEach( loadJSONFiles);
+$.ajax({
+	url: headUrl + htmlList[2],
+	success: function(data) {
+		$('#results_two').load('pagetwo.html');
+		//document.body.appendChildren(data);
+	},
+	error: function(data) {
+	  console.error(data);
+	}
+});
